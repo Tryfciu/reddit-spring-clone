25188e2c2d0da033b827f80af23dec022c9ab293
Paweł Tryfon <pawelxtryfon@gmail.com>
2020-10-04 16:46:51 +0200

    Init commit

---
+++ src/test/java/pwxon/twitterclone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pwxon.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
