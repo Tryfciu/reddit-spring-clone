26cc7b71dc29f4a4a965b112e24b605ad35d2b34
Paweł Tryfon <pawelxtryfon@gmail.com>
2020-10-04 21:40:35 +0200

    Fix import section

--- src/main/java/pwxon/redditclone/repository/CommentRepository.java
+++ src/main/java/pwxon/redditclone/repository/CommentRepository.java
@@ -3,7 +3,6 @@ package pwxon.redditclone.repository;
 import org.springframework.stereotype.Repository;
 import pwxon.redditclone.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
 @Repository
--- src/main/java/pwxon/redditclone/repository/VoteRepository.java
+++ src/main/java/pwxon/redditclone/repository/VoteRepository.java
@@ -3,7 +3,6 @@ package pwxon.redditclone.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pwxon.redditclone.model.Vote;
-
 import java.util.UUID;
 
 @Repository
